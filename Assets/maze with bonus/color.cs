e5536fe0cb7cae711205a3777f05bf7fa413cb69
DeansCollective <deantrojanowski@gmail.com>
2025-11-19 18:54:34 -0500

    assignment one

---
+++ Assets/maze with bonus/color.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SetColorOnStart : MonoBehaviour
+{
+    public Renderer targetRenderer;
+    public Color newColor = Color.green;   // <-- your static color
+
+    void Start()
+    {
+        if (targetRenderer == null)
+            targetRenderer = GetComponent<Renderer>();
+
+        targetRenderer.material.color = newColor;
+    }
+}
